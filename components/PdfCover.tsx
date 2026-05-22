7a8753b74d62944d095d223c3c204e39b5114670
Banolite Dev <developer@banolite.com>
2026-05-22 19:22:11 +0100

    Fix react-pdf CSS import paths for v10+

--- components/PdfCover.tsx
+++ components/PdfCover.tsx
@@ -1,8 +1,8 @@
 'use client';
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 // Set worker path to local if needed or cdn
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
